f8b88f8b8fd6853bc2cd08a16ae29d3ca2a684c2
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-03-31 22:25:46 +0700

    Make function greeting src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-const greeting = () => {
+const greeting = (name) => {
 const userName = readlineSync.question('May I have your name? ');
 console.log('Hello, ' + userName + '!');
 };
