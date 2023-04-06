c4b52940b885b0e15d6aa600b6b25bcc341a1f50
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-06 22:30:00 +0700

    Update src/even.js

---
+++ src/even.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (number) => number % 2 === 0;
+
+const playGame = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const gameNumber = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = (isEven(gameNumber)) ? "'yes'" : "'no'";
+    const userAnswer = readlineSync.question(`${'Question: '}${gameNumber}\n${'Your answer: '}`);
+    if (gameNumber % 2 === 0 && userAnswer === 'yes') {
+      console.log('Correct!');
+    } else if (gameNumber % 2 !== 0 && userAnswer === 'no') {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+export default playGame;
