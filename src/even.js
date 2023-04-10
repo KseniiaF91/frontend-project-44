346fc388822444246f393d5727c83ea12ba74a97
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-10 21:46:19 +0700

    Update src/even.js

--- src/even.js
+++ src/even.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync';
 const isEven = (number) => number % 2 === 0;
 
 const playGameEven = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
 
