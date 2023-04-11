23293ab68b8c1902f74df83642180761b0c75f8d
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-11 21:05:12 +0700

    Fix src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,19 +1,15 @@
 import readlineSync from 'readline-sync';
 
-const isEven = (number) => number % 2 === 0;
-
 const playGameEven = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
-
+  const isEven = (number) => number % 2 === 0;
   for (let i = 0; i < 3; i += 1) {
     const gameNumber = Math.floor(Math.random() * 100) + 1;
     const correctAnswer = (isEven(gameNumber)) ? 'yes' : 'no';
     const userAnswer = readlineSync.question(`${'Question: '}${gameNumber}\n${'Your answer: '}`);
-    if (gameNumber % 2 === 0 && userAnswer === 'yes') {
-      console.log('Correct!');
-    } else if (gameNumber % 2 !== 0 && userAnswer === 'no') {
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
