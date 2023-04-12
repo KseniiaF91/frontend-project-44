e1fa1271724e944831d0c1dd16556c4570ffe9b3
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-12 21:36:20 +0700

    Update  src/index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+
+const mainGameLogic = (gameTask, gameRules) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameTask);
+
+  for (let i = 0; i < 3; i += 1) {
+    const [correctAnswer, gameQuestion] = gameRules();
+    console.log(gameQuestion);
+    const userAnswer = readlineSync.question(`${'Your answer: '}`);
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+export default mainGameLogic;
