ba0e6883a28829cacfeeb5c0c38d6f39c29741e4
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-12 21:35:47 +0700

    Update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,21 +1,15 @@
-import readlineSync from 'readline-sync';
+import mainGameLogic from '../index.js';
 
 const playGameEven = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
-  const isEven = (number) => number % 2 === 0;
-  for (let i = 0; i < 3; i += 1) {
+  const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+  const gameRules = () => {
+    const isEven = (number) => number % 2 === 0;
     const gameNumber = Math.floor(Math.random() * 100) + 1;
     const correctAnswer = (isEven(gameNumber)) ? 'yes' : 'no';
-    const userAnswer = readlineSync.question(`${'Question: '}${gameNumber}\n${'Your answer: '}`);
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+    const gameQuestion = (`${'Question: '}${gameNumber}`);
+    return [correctAnswer, gameQuestion];
+  };
+  mainGameLogic(gameTask, gameRules);
 };
 export default playGameEven;
