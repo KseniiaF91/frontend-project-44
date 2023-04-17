089e9be3514c713e8cfb4e652714f72ec295465f
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-17 21:54:02 +0700

    Update progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,12 @@
+import mainGameLogic from '../index.js';
+import randomNumber from '../randomnum.js';
+
+const playGameProgression = () => {
+  const gameTask = 'What number is missing in the progression?';
+
+  const gameRules = () => {
+
+  };
+  mainGameLogic(gameTask, gameRules);
+};
+export default playGameProgression;
