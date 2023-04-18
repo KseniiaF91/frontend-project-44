4586ef1091f5210ea290576b0d87f59f1bf4d08f
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-18 21:51:40 +0700

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,12 +1,26 @@
 import mainGameLogic from '../index.js';
-import randomNumber from '../randomnum.js';
 
 const playGameProgression = () => {
   const gameTask = 'What number is missing in the progression?';
 
   const gameRules = () => {
+    const arr = [];
+    const length = 10;
+    const difference = Math.floor(Math.random() * 10) + 1;
+    arr[0] = Math.floor(Math.random() * 10) + 1;
 
+    for (let i = 1; i < length; i += 1) {
+      arr[i] = arr[i - 1] + difference;
+    }
+    const hiddenSymbol = arr.splice((Math.floor(Math.random() * 10) + 1), 1, '..');
+
+    const gameQuestion = (`${'Question: '}${arr}`);
+
+    const correctAnswer = hiddenSymbol;
+
+    return [gameQuestion, correctAnswer.toString()];
   };
+
   mainGameLogic(gameTask, gameRules);
 };
 export default playGameProgression;
