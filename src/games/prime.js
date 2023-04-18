b858bf47daf91ac98032a17e0086d9563c1bd009
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-18 23:44:53 +0700

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,19 +5,19 @@ const playGamePrime = () => {
   const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const gameRules = () => {
-    const gameNumber = randomNumber();
-    const gameQuestion = (`${'Question: '}${gameNumber}`);
-    const isPrime = () => {
-      if (gameNumber <= 1) {
+    const isPrime = (number) => {
+      if (number <= 1) {
         return false;
       }
-      for (let i = 2; i <= Math.sqrt(gameNumber); i += 1) {
-        if (gameNumber % i === 0) {
+      for (let i = 2; i <= Math.sqrt(number); i += 1) {
+        if (number % i === 0) {
           return false;
         }
       }
       return true;
     };
+    const gameNumber = randomNumber();
+    const gameQuestion = (`${'Question: '}${gameNumber}`);
     const correctAnswer = (isPrime(gameNumber)) ? 'yes' : 'no';
     return [gameQuestion, correctAnswer];
   };
