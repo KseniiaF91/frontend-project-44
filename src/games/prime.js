8fe5892e6327b3ac6c1ad521050399e25fe269c2
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-19 20:32:51 +0700

    Fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,18 +5,18 @@ const playGamePrime = () => {
   const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const gameRules = () => {
-    const isPrime = (number) => {
-      if (number <= 1) {
+    const gameNumber = randomNumber();
+    const isPrime = () => {
+      if (gameNumber <= 1) {
         return false;
       }
-      for (let i = 2; i <= Math.sqrt(number); i += 1) {
-        if (number % i === 0) {
+      for (let i = 2; i <= Math.sqrt(gameNumber); i += 1) {
+        if (gameNumber % i === 0) {
           return false;
         }
       }
       return true;
     };
-    const gameNumber = randomNumber();
     const gameQuestion = (`Question: ${gameNumber}`);
     const correctAnswer = (isPrime(gameNumber)) ? 'yes' : 'no';
     return [gameQuestion, correctAnswer];
