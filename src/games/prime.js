da25d07c06779b6fd04620fc7152dec2111d6878
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-20 18:53:39 +0700

    Fix name prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,16 +1,16 @@
-import mainGameLogic from '../index.js';
+import playGame from '../index.js';
 import randomNumber from '../randomnum.js';
 import isPrime from '../src/isPrime.js';
 
 const playGamePrime = () => {
   const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-  const gameRules = () => {
+  const generateRoundData = () => {
     const gameNumber = randomNumber();
     const gameQuestion = (`Question: ${gameNumber}`);
     const correctAnswer = (isPrime(gameNumber)) ? 'yes' : 'no';
     return [gameQuestion, correctAnswer];
   };
-  mainGameLogic(gameTask, gameRules);
+  playGame(gameTask, generateRoundData);
 };
 export default playGamePrime;
