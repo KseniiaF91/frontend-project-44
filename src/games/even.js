d9621a821a8e0b9a793e444978f228f77507149d
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-04-20 18:52:44 +0700

    Fix name even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,16 @@
-import mainGameLogic from '../index.js';
+import playGame from '../index.js';
 import randomNumber from '../randomnum.js';
 
 const playGameEven = () => {
   const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  const gameRules = () => {
+  const generateRoundData = () => {
     const isEven = (number) => number % 2 === 0;
     const gameNumber = randomNumber();
     const gameQuestion = (`Question: ${gameNumber}`);
     const correctAnswer = (isEven(gameNumber)) ? 'yes' : 'no';
     return [gameQuestion, correctAnswer];
   };
-  mainGameLogic(gameTask, gameRules);
+  playGame(gameTask, generateRoundData);
 };
 export default playGameEven;
